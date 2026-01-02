1ebd7313fcbf6ee4571c21d2513ac36a40e472d1
vamshi0892 <vamshi0892@gmail.com>
2026-01-01 20:25:07 -0500

    modified root module main.tf and network module main.tf files

--- az_modules_network/env/dev/main.tf
+++ az_modules_network/env/dev/main.tf
@@ -9,8 +9,8 @@ resource "azurerm_resource_group" "rg" {
 
 module "network" {
   source              = "../../modules/network"
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 
   vnet_name     = var.vnet_name
   address_space = var.address_space
